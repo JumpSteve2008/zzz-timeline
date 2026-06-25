21856a78582a653b0d170f19925e4b6e72a1afaf
广林 胥 <9965273@qq.com>
2026-06-26 00:51:19 +0800

    Download images locally, update to local paths

--- sw.js
+++ sw.js
@@ -1,9 +1,27 @@
-var CACHE = "zzz-v1";
+var CACHE = "zzz-v2";
 var ASSETS = [
-    "/index.html",
-    "/css/style.css",
-    "/js/main.js",
-    "/manifest.json"
+    "./index.html",
+    "./css/style.css",
+    "./js/main.js",
+    "./manifest.json",
+    "./img/v1.0.png",
+    "./img/v1.1.png",
+    "./img/v1.2.png",
+    "./img/v1.3.png",
+    "./img/v1.4.png",
+    "./img/v1.5.png",
+    "./img/v1.6.png",
+    "./img/v1.7.png",
+    "./img/v2.0.png",
+    "./img/v2.1.png",
+    "./img/v2.2.png",
+    "./img/v2.3.png",
+    "./img/v2.4.png",
+    "./img/v2.5.png",
+    "./img/v2.6.png",
+    "./img/v2.7.png",
+    "./img/v2.8.png",
+    "./img/v3.0.png"
 ];
 
 self.addEventListener("install", function (e) {
